43b589348bae4ecabd0e55b61ffcde324e036b2c
Joaquin Tissera <phoenix_css@hotmail.com>
2024-09-13 18:15:57 -0300

    Apply Rufo's default formatting

--- app.rb
+++ app.rb
@@ -1,66 +1,66 @@
 # Correr app: ruby app.rb
 # http://127.0.0.1:3000/
 
-require 'sinatra'
-require 'sinatra/activerecord'
-require 'sinatra/cors'
-require 'sinatra/json'
-require 'byebug'
-require 'fileutils'
-require 'dotenv/load'
-require 'pdf-reader'
-require 'json'
-require 'openai'
+require "sinatra"
+require "sinatra/activerecord"
+require "sinatra/cors"
+require "sinatra/json"
+require "byebug"
+require "fileutils"
+require "dotenv/load"
+require "pdf-reader"
+require "json"
+require "openai"
 
-require './config/environment'
+require "./config/environment"
 
-set :allow_origin, 'http://127.0.0.1:3000'
+set :allow_origin, "http://127.0.0.1:3000"
 set :port, 3000
 enable :sessions
 
 # Variable global a los metodos utilizada en POST Register para manejar errores
-error_registration = ''
+error_registration = ""
 
 before do
   @isAnUserPresent = session[:isAnUserPresent] || false
 end
 
-get '/' do
+get "/" do
   erb :index
 end
 
-get '/register' do
+get "/register" do
   erb :register
 end
 
-get '/login' do
+get "/login" do
   erb :login
 end
 
-get '/error-register' do
+get "/error-register" do
   case error_registration
-  when 'missing_fields'
+  when "missing_fields"
     erb :error_missing_fields
-  when 'user_exists'
+  when "user_exists"
     erb :error_user_exists
-  when 'user_not_persisted'
+  when "user_not_persisted"
     erb :error_registration_failed
   else
-    redirect '/register'
+    redirect "/register"
   end
 end
 
-get '/logout' do
+get "/logout" do
   session[:isAnUserPresent] = false
-  redirect '/'
+  redirect "/"
 end
 
-get '/practice' do
-  redirect '/' unless session[:isAnUserPresent]
+get "/practice" do
+  redirect "/" unless session[:isAnUserPresent]
   erb :practice
 end
 
-post '/login' do
+post "/login" do
   if !session[:isAnUserPresent]
     username_or_email = params[:username_or_email]
     password = params[:password]
@@ -69,70 +69,70 @@ post '/login' do
       @user = User.find_by(username: username_or_email, password: password) || User.find_by(email: username_or_email, password: password)
       if @user
         session[:isAnUserPresent] = true
-        redirect '/'
+        redirect "/"
       else
-        @error = 'No se encontró el usuario o el correo, o la contraseña es incorrecta!'
+        @error = "No se encontró el usuario o el correo, o la contraseña es incorrecta!"
         erb :login
       end
     else
-      @error = 'Ingrese el nombre de usuario o correo electronico y la contraseña!'
+      @error = "Ingrese el nombre de usuario o correo electronico y la contraseña!"
       erb :login
     end
   else
-    @error = 'Para entrar en una cuenta primero se debe salir de la cuenta actual!'
+    @error = "Para entrar en una cuenta primero se debe salir de la cuenta actual!"
     erb :login
   end
 end
 
-post '/register' do
+post "/register" do
   username = params[:username]
   name = params[:name]
-  lastname = params[:lastname] || 'Apellido No registrado'
-  cellphone = params[:cellphone] || 'Celular No registrado'
+  lastname = params[:lastname] || "Apellido No registrado"
+  cellphone = params[:cellphone] || "Celular No registrado"
   email = params[:email]
   password = params[:password]
 
   if username.nil? || name.nil? || email.nil? || password.nil? || username.strip.empty? || name.strip.empty? || email.strip.empty? || password.strip.empty?
-    error_registration = 'missing_fields'
-    redirect '/error-register'
+    error_registration = "missing_fields"
+    redirect "/error-register"
     # Entra solamente por aca cuando se ponen espacios, porque el ".nil?" ya lo controla el form en el ERB con la clausula "required"
   else
     @user = User.find_by(username: username) || User.find_by(email: email)
     if @user
-      error_registration = 'user_exists'
-      redirect '/error-register'
+      error_registration = "user_exists"
+      redirect "/error-register"
     else
       @user = User.create(username: username, name: name, lastname: lastname, cellphone: cellphone, email: email,
                           password: password)
       if @user.persisted?
-        error_registration = ''
+        error_registration = ""
         session[:isAnUserPresent] = true
-        redirect '/'
+        redirect "/"
       else
-        error_registration = 'user_not_persisted'
-        redirect '/error-register'
+        error_registration = "user_not_persisted"
+        redirect "/error-register"
       end
     end
   end
 end
 
-post '/logout' do
+post "/logout" do
   session[:isAnUserPresent] = false
-  redirect '/'
+  redirect "/"
 end
 
-post '/practice' do
-  logger.info 'Received request to generate quiz'
+post "/practice" do
+  logger.info "Received request to generate quiz"
   logger.info "Params: #{params.inspect}"
 
   file = fetch_file(params)
   return file unless file.is_a?(Tempfile)
 
   full_text = extract_text_from_pdf(file)
-  return json_error('Failed to extract text from PDF', 500) if full_text.empty?
+  return json_error("Failed to extract text from PDF", 500) if full_text.empty?
 
   @questions = generate_questions(full_text)
-  return json_error('Failed to generate quiz', 503) unless @questions
+  return json_error("Failed to generate quiz", 503) unless @questions
 
   session[:questions] = @questions # Guardamos las preguntas en la sesión
   session[:current_question_index] = 0 # Iniciamos en la primera pregunta
@@ -142,25 +142,24 @@ post '/practice' do
   erb :question
 end
 
-
-post '/next_question' do
+post "/next_question" do
   @questions = session[:questions] # Recuperamos las preguntas de la sesión
   current_question_index = session[:current_question_index] # Recuperamos el índice
 
   if @questions.nil? || current_question_index.nil?
-    @error = 'No se encontraron preguntas o índice. Por favor, sube un PDF para generar el quiz.'
-    redirect '/practice'
+    @error = "No se encontraron preguntas o índice. Por favor, sube un PDF para generar el quiz."
+    redirect "/practice"
   end
 
   selected_answer = params[:selected_option]
-  correct_answer = @questions[current_question_index]['answer'] if @questions[current_question_index]
+  correct_answer = @questions[current_question_index]["answer"] if @questions[current_question_index]
 
   if selected_answer == correct_answer
-    @message = '¡Correcto!'
-    @message_class = 'correct'
+    @message = "¡Correcto!"
+    @message_class = "correct"
   else
     @message = "Incorrecto. La respuesta correcta era: #{correct_answer}"
-    @message_class = 'incorrect'
+    @message_class = "incorrect"
   end
 
   # Guardamos la respuesta seleccionada en answered_questions
@@ -175,30 +174,29 @@ post '/next_question' do
   else
     # Contamos las respuestas correctas
     @correct_answers = session[:answered_questions].each_with_index.count do |answer, index|
-      answer == @questions[index]['answer']
+      answer == @questions[index]["answer"]
     end
     @incorrect_answers = @questions.size - @correct_answers
 
-    @message = 'Has completado todas las preguntas.'
+    @message = "Has completado todas las preguntas."
     erb :quiz_complete
   end
 end
 
-
 # Initializes the OpenAI client
 def client
-  options = { access_token: ENV['TOKEN_OPENAI'], log_errors: true }
+  options = { access_token: ENV["TOKEN_OPENAI"], log_errors: true }
   @client ||= OpenAI::Client.new(**options)
 end
 
 # Fetches the file from a file upload
 def fetch_file(params)
   if params[:file] && params[:file][:tempfile]
-    logger.info 'Successfully received file from tempfile'
+    logger.info "Successfully received file from tempfile"
     params[:file][:tempfile]
   else
-    logger.error 'No file provided'
-    json_error('No file provided', 400)
+    logger.error "No file provided"
+    json_error("No file provided", 400)
   end
 end
 
@@ -211,14 +209,14 @@ def generate_questions(full_text)
 
   response = client.chat(
     parameters: {
-      model: 'gpt-4o-mini',
+      model: "gpt-4o-mini",
       messages: [
-        { role: 'system', content: prompt },
-        { role: 'user', content: full_text }
+        { role: "system", content: prompt },
+        { role: "user", content: full_text },
       ],
       max_tokens: 3000,
-      temperature: 0.5
-    }
+      temperature: 0.5,
+    },
   )
 
   # Ver que devolvio GPT por consola
@@ -232,9 +230,9 @@ end
 
 # Parses the response from the AI and extracts the questions
 def parse_response(response)
-  content = response.dig('choices', 0, 'message', 'content')
-  content.gsub!(/^```json\n/, '').gsub!(/\n```$/, '')
-  puts "Raw response: #{content}" 
+  content = response.dig("choices", 0, "message", "content")
+  content.gsub!(/^```json\n/, "").gsub!(/\n```$/, "")
+  puts "Raw response: #{content}"
   JSON.parse(content)
 rescue StandardError => e
   logger.error "Failed to parse AI response: #{e.message}"
@@ -247,7 +245,7 @@ def extract_text_from_pdf(file)
   pdf.pages.map(&:text).join
 rescue StandardError => e
   logger.error "Direct PDF text extraction failed: #{e.message}"
-  ''
+  ""
 end
 
 # Helper method for JSON error responses
