30b3b182a41e7d4845f8b57c182005b19a1d2111
NikyRosatti <nicollerosatti@gmail.com>
2024-10-07 21:47:46 -0300

    [#188362185] Updating attributes, to manage statistics per question

--- db/migrate/20240827152734_create_questions.rb
+++ db/migrate/20240827152734_create_questions.rb
@@ -3,7 +3,8 @@ class CreateQuestions < ActiveRecord::Migration[7.1]
     create_table :questions do |t|
       t.string :content
       t.references :document, foreign_key: true
-
+      t.integer :number_answers_answered, default: 0
+      t.integer :correct_answers_cant, default: 0
       t.timestamps
     end
   end
