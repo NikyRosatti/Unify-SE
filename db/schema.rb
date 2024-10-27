83e0eca80da5f6d465e5c8ab5ef00375a325a20b
NikyRosatti <nicollerosatti@gmail.com>
2024-10-27 18:34:31 -0300

    Branch Update

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,72 +12,72 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_12_110735) do
-  create_table "answers", force: :cascade do |t|
-    t.integer "question_id"
-    t.integer "option_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["option_id"], name: "index_answers_on_option_id"
-    t.index ["question_id"], name: "index_answers_on_question_id"
+ActiveRecord::Schema[7.2].define(version: 20_241_012_110_735) do # rubocop:disable Metrics/BlockLength
+  create_table 'answers', force: :cascade do |t|
+    t.integer 'question_id'
+    t.integer 'option_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['option_id'], name: 'index_answers_on_option_id'
+    t.index ['question_id'], name: 'index_answers_on_question_id'
   end
 
-  create_table "documents", force: :cascade do |t|
-    t.string "filename"
-    t.binary "filecontent"
-    t.string "file_hash"
-    t.date "uploaddate"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["file_hash"], name: "index_documents_on_file_hash", unique: true
+  create_table 'documents', force: :cascade do |t|
+    t.string 'filename'
+    t.binary 'filecontent'
+    t.string 'file_hash'
+    t.date 'uploaddate'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['file_hash'], name: 'index_documents_on_file_hash', unique: true
   end
 
-  create_table "favorites", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "document_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["document_id"], name: "index_favorites_on_document_id"
-    t.index ["user_id"], name: "index_favorites_on_user_id"
+  create_table 'favorites', force: :cascade do |t|
+    t.integer 'user_id', null: false
+    t.integer 'document_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['document_id'], name: 'index_favorites_on_document_id'
+    t.index ['user_id'], name: 'index_favorites_on_user_id'
   end
 
-  create_table "options", force: :cascade do |t|
-    t.string "content"
-    t.integer "question_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["question_id"], name: "index_options_on_question_id"
+  create_table 'options', force: :cascade do |t|
+    t.string 'content'
+    t.integer 'question_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['question_id'], name: 'index_options_on_question_id'
   end
 
-  create_table "questions", force: :cascade do |t|
-    t.string "content"
-    t.integer "document_id"
-    t.integer "number_answers_answered", default: 0
-    t.integer "correct_answers_cant", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["document_id"], name: "index_questions_on_document_id"
+  create_table 'questions', force: :cascade do |t|
+    t.string 'content'
+    t.integer 'document_id'
+    t.integer 'number_answers_answered', default: 0
+    t.integer 'correct_answers_cant', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['document_id'], name: 'index_questions_on_document_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "name"
-    t.string "lastname"
-    t.string "cellphone"
-    t.string "email"
-    t.string "password"
-    t.date "b_day"
-    t.string "gender"
-    t.integer "is_admin", default: 0
-    t.integer "correct_answers", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'username'
+    t.string 'name'
+    t.string 'lastname'
+    t.string 'cellphone'
+    t.string 'email'
+    t.string 'password'
+    t.date 'b_day'
+    t.string 'gender'
+    t.integer 'is_admin', default: 0
+    t.integer 'correct_answers', default: 0
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "answers", "options"
-  add_foreign_key "answers", "questions"
-  add_foreign_key "favorites", "documents"
-  add_foreign_key "favorites", "users"
-  add_foreign_key "options", "questions"
-  add_foreign_key "questions", "documents"
+  add_foreign_key 'answers', 'options'
+  add_foreign_key 'answers', 'questions'
+  add_foreign_key 'favorites', 'documents'
+  add_foreign_key 'favorites', 'users'
+  add_foreign_key 'options', 'questions'
+  add_foreign_key 'questions', 'documents'
 end
